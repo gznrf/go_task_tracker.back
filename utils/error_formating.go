8a54ce4e6632d47d68a774e7e813c075f60c33d5
gznrf <iq703i@example.com>
2025-11-20 16:28:40 +0300

    feat()

--- utils/error_formating.go
+++ utils/error_formating.go
@@ -8,16 +8,19 @@ import (
 )
 
 func WriteJson(w http.ResponseWriter, status int, v any) error {
-	w.Header().Add("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "http://195.133.77.31:3321")
+	w.Header().Set("Access-Control-Allow-Credentials", "true")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE, OPTIONS")
+
+	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(status)
 
 	return json.NewEncoder(w).Encode(v)
 }
 
 func WriteError(w http.ResponseWriter, status int, err error) {
-	if err := WriteJson(w, status, map[string]string{"error": err.Error()}); err != nil {
-		WriteError(w, http.StatusInternalServerError, err)
-	}
+	_ = WriteJson(w, status, map[string]string{"error": err.Error()})
 }
 
 func Validate(s any) error {
